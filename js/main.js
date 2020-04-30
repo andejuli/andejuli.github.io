54f5a191204f0504af12c997241740e4e139c811
andejuli <techtrek@hotmail.com>
2020-04-30 07:35:52 -0600

    design

--- js/main.js
+++ js/main.js
@@ -1,2 +1,6 @@
-let currentdate = document.lastModified;
-        document.getElementById("modified").textContent = currentdate;
+/*let currentdate = document.lastModified;
+document.getElementById("modified").textContent = currentdate;*/
+
+var options = {month: "numeric", day: "numeric", year: "numeric", time: "numeric"};
+var d = new Date().toLocaleDateString('en-US', options);
+document.getElementById("modified").textContent = d ;
