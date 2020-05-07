26ac9580f919bdb5b2ccf0cb7e4b49546b4b6848
andejuli <techtrek@hotmail.com>
2020-05-07 10:02:33 -0600

    weather plan added

--- js/main.js
+++ js/main.js
@@ -1,6 +1,3 @@
 let currentdate = document.lastModified;
 document.getElementById("modified").innerHTML = currentdate;
 
-/*var options = {month: "numeric", day: "numeric", year: "numeric", hour: "numeric", minute: "numeric"};
-var d = new Date().toLocaleDateString('en-US', options);
-document.getElementById("modified").textContent = d;*/
