19df3d61e30f137de479e5b488c3dd21261e1348
andejuli <techtrek@hotmail.com>
2022-03-05 18:37:05 -0700

    new api key

--- wint22/chamber/week10/js/weather-api1.js
+++ wint22/chamber/week10/js/weather-api1.js
@@ -1,4 +1,4 @@
-const apiURL = 'api.openweathermap.org/data/2.5/weather?id=5596475&appid=e6b8d481b3a497c7db7521cc35f46dda';
+const apiURL = 'api.openweathermap.org/data/2.5/weather?id=5596475&appid=bc4bb35104ef8c70efd2262a7c121cfc';
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
